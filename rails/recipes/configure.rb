f2dbd5073413a1930ce25f9ff845c27cbef651f9
Leszek Zalewski <pb.zalewski.leszek@gmail.com>
2012-10-01 23:01:24 +0200

    removed useless override of deploy variable

--- rails/recipes/configure.rb
+++ rails/recipes/configure.rb
@@ -1,15 +1,13 @@
 include_recipe "deploy"
 
 node[:deploy].each do |application, deploy|
-  deploy = node[:deploy][application]
-
   execute "restart Rails app #{application}" do
     cwd deploy[:current_path]
     command node[:scalarium][:rails_stack][:restart_command]
     action :nothing
   end
 
-  node[:deploy][application][:database][:adapter] = Scalarium::RailsConfiguration.determine_database_adapter(application, node[:deploy][application], "#{node[:deploy][application][:deploy_to]}/current", :force => node[:force_database_adapter_detection])
+  deploy[:database][:adapter] = Scalarium::RailsConfiguration.determine_database_adapter(application, deploy, "#{deploy[:deploy_to]}/current", :force => node[:force_database_adapter_detection])
 
   template "#{deploy[:deploy_to]}/shared/config/database.yml" do
     source "database.yml.erb"
